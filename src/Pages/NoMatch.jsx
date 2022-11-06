b2cf2842abdfe032f6c5371201a9312ff81cc089
pscon <ekunolapaul@gmail.com>
2022-11-06 23:14:16 +0100

    added readme file content

--- src/Pages/NoMatch.jsx
+++ src/Pages/NoMatch.jsx
@@ -16,7 +16,7 @@ function NoMatch() {
                 <div className="contant_box_404">
                   <h3 className="h2">Look like you're lost</h3>
 
-                  <p>the page you are looking for is not avaible!</p>
+                  <p>the page you are looking for is not available!</p>
 
                   <Link to="/" className="link_404">
                     Go to Home{" "}
